ef54f498e401b1bafb606768daa6daa4f8bf3177
ATLAS-B28 <adityacbhambere28@gmail.com>
2025-09-23 10:04:10 +0530

    Adding HSM

---
+++ app/customer.py
@@ -0,0 +1,71 @@
+from dbconfig import connect_to_mysql
+
+class Customer:
+    def __init__(self, customer_id=None, phone=None, email=None, address=None, no_of_guests=None):
+        self.customer_id = customer_id
+        self.phone = phone
+        self.email = email
+        self.address = address
+        self.no_of_guests = no_of_guests
+    
+    def save_or_update(self):
+        connection, cursor = connect_to_mysql()
+        try:
+            if self.customer_id is None:
+                query = "INSERT INTO customer (address, phone, email, no_of_guests) VALUES (%s, %s, %s, %s)"
+                values = (self.address, self.phone, self.email, self.no_of_guests)
+                cursor.execute(query, values)
+                self.customer_id = cursor.lastrowid
+            else:
+                query = "UPDATE customer SET address=%s, phone=%s, email=%s, no_of_guests=%s WHERE customer_id=%s"
+                values = (self.address, self.phone, self.email, self.no_of_guests, self.customer_id)
+                cursor.execute(query, values)
+            connection.commit()
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            cursor.close()
+            connection.close()
+    
+    @classmethod
+    def get_by_id(cls, customer_id):
+        connection, cursor = connect_to_mysql()
+        try:
+            query = "SELECT * FROM customer WHERE customer_id = %s"
+            cursor.execute(query, (customer_id,))
+            result = cursor.fetchone()
+            if result:
+                return cls(*result)
+            return None
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            cursor.close()
+            connection.close()
+    
+    @classmethod
+    def get_all(cls):
+        connection, cursor = connect_to_mysql()
+        try:
+            query = "SELECT * FROM customer"
+            cursor.execute(query)
+            results = cursor.fetchall()
+            return [cls(*result) for result in results]
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            cursor.close()
+            connection.close()
+    
+    def delete(self):
+        if self.customer_id:
+            connection, cursor = connect_to_mysql()
+            try:
+                query = "DELETE FROM customer WHERE customer_id = %s"
+                cursor.execute(query, (self.customer_id,))
+                connection.commit()
+            except Exception as e:
+                print(f"Error: {e}")
+            finally:
+                cursor.close()
+                connection.close()
---
+++ app/main.py
@@ -0,0 +1,2 @@
+#here we will be doing all the operations
+#a main function with menu driven capablity
